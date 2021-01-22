5f95f394fc5bbe81e13004b92164a1793e98e477
PratikshaKarwa <PratikshaKarwa@LAPTOP-7CK7JTNB>
2021-01-22 10:33:56 +0530

    this is 2nd commit

---
+++ FlightSystem/src/com/mvc/bean/BookBean.java
@@ -0,0 +1,49 @@
+package com.mvc.bean;
+
+public class BookBean {
+
+	private String flightcode;
+	private String passengername;
+	private String source;
+	private String destination;
+	private String departure;
+	private String payment;
+	public 	String getFlightcode() {
+		return flightcode;
+	}
+	public void setFlightcode(String flightcode) {
+		this.flightcode = flightcode;
+	}
+	public String getPassengername() {
+		return passengername;
+	}
+	public void setPassengername(String passengername) {
+		this.passengername = passengername;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getDeparture() {
+		return departure;
+	}
+	public void setDeparture(String departure) {
+		this.departure = departure;
+	}
+	public String getPayment() {
+		return payment;
+	}
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+	
+	
+}

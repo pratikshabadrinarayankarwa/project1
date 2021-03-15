0ab323e7070c459b3d6107c0dcf7363976a01fea
PratikshaKarwa <PratikshaKarwa@LAPTOP-7CK7JTNB>
2021-03-15 12:33:08 +0530

    this is our bitbucket project

---
+++ tradingapplicationsystemdemo-5/src/main/java/com/atyeti/tradingapplicationsystemdemo1/Tradingapplicationsystemdemo5Application.java
@@ -0,0 +1,13 @@
+package com.atyeti.tradingapplicationsystemdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tradingapplicationsystemdemo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tradingapplicationsystemdemo5Application.class, args);
+	}
+
+}
---
+++ tradingapplicationsystemdemo-5/src/test/java/com/atyeti/tradingapplicationsystemdemo1/Tradingapplicationsystemdemo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atyeti.tradingapplicationsystemdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tradingapplicationsystemdemo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
